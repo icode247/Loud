845c874c81af55a2a6c6b1f7db9649312ad45f22
icode247 <zionekekenta@gmail.com>
2021-06-04 19:19:48 -0700

    added auth

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 
 module.exports =  {
     SECRET_KEY : process.env.SECRET_KEY,
-    dbConnectingString : "mongodb+srv://todos:f7ih9bIXjqZlraMW@todos.jmdue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    dbConnectingString : 'mongodb://localhost:27017/loud'
 }
-//dbConnectingString : 'mongodb://localhost:27017/loud'
